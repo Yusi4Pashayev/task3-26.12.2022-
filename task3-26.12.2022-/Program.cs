3c7d07adcf6e803c905630f8969293275a1812d3
yusi4 <yusi4@DESKTOP-E80JV74>
2022-12-28 02:18:09 +0400

    Add project files.

---
+++ task3-26.12.2022-/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace task3_26._12._2022_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //3. 5 reqemli eded verilib.Bu ededin evveline 4 reqemini , axirinada 2 reqemini artir.
+            //Giris: 16538
+            //Cixis: 4165382
+
+            Console.Write("5 reqemli eded daxil edin:");
+            int a = int.Parse(Console.ReadLine());
+            if(a>=10000 && a<=99999)
+            {
+                a = a + 400000;
+                a = a * 10 + 2;
+                Console.Write(a);
+
+            }
+            else Console.WriteLine("daxil etdiyiniz eded 5 reqemli deyil!");
+        }
+    }
+}
